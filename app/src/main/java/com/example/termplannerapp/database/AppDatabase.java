5df7053ac11c6e9775c452906f4ddab6eed1bb64
Russell Anderson <russellanderson2000@gmail.com>
2020-02-24 20:36:22 -0500

    built type converter class for passing dates between java and sqllite

---
+++ app/src/main/java/com/example/termplannerapp/database/AppDatabase.java
@@ -0,0 +1,31 @@
+package com.example.termplannerapp.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+
+@Database(entities = {TermEntity.class}, version = 1)
+@TypeConverters(DateConverter.class)
+
+public abstract class AppDatabase extends RoomDatabase {
+    public static final String DATABASE_NAME = "AppDatabase.db";
+    public static volatile AppDatabase instance;
+    public static final Object LOCK = new Object();
+
+    public abstract TermDao termDao();
+
+    public static AppDatabase getInstance(Context context) {
+        if (instance == null) {
+            synchronized (LOCK) {
+                if (instance == null) {
+                    instance = Room.databaseBuilder(context.getApplicationContext(),
+                            AppDatabase.class, DATABASE_NAME).build();
+                }
+            }
+        }
+        return instance;
+    }
+}
