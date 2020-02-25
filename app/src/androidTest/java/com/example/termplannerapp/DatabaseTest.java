3a2e98111f353f5c5de94694c07f210f3fbfc7cb
Russell Anderson <russellanderson2000@gmail.com>
2020-02-25 12:29:03 -0500

    removed unused imports

--- app/src/androidTest/java/com/example/termplannerapp/DatabaseTest.java
+++ app/src/androidTest/java/com/example/termplannerapp/DatabaseTest.java
@@ -17,7 +17,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
 public class DatabaseTest {
