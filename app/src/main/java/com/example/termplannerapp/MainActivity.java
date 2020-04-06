78a0db1e3a66fa78b29dd2c69b2543bd839eae0a
Russell Anderson <russellanderson2000@gmail.com>
2020-04-06 19:57:01 -0400

    refactoring main activity to allow for buttons to link to terms, courses etc...

--- app/src/main/java/com/example/termplannerapp/MainActivity.java
+++ app/src/main/java/com/example/termplannerapp/MainActivity.java
@@ -1,42 +1,18 @@
 package com.example.termplannerapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.termplannerapp.database.TermEntity;
-import com.example.termplannerapp.ui.TermsAdapter;
 import com.example.termplannerapp.viewmodel.MainViewModel;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.term_recycler_view)
-    RecyclerView mTermRecyclerView;
-
-    @OnClick(R.id.fab)
-    void fabClickHandler() {
-        Intent intent = new Intent(this, TermEditorActivity.class);
-        startActivity(intent);
-    }
-
-    private List<TermEntity> termsData = new ArrayList<>();
-    private TermsAdapter mTermsAdapter;
     private MainViewModel mViewModel;
 
 
@@ -46,43 +22,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Terms");
 
         ButterKnife.bind(this);
-        initRecyclerView();
-        initViewModel();
-    }
-
-    private void initViewModel() {
-
-        final Observer<List<TermEntity>> termsObserver = new Observer<List<TermEntity>>() {
-            @Override
-            public void onChanged(List<TermEntity> termEntities) {
-                termsData.clear();
-                termsData.addAll(termEntities);
-
-                if (mTermsAdapter == null) {
-                    mTermsAdapter = new TermsAdapter(termsData,
-                            MainActivity.this);
-                    mTermRecyclerView.setAdapter(mTermsAdapter);
-                } else {
-                    mTermsAdapter.notifyDataSetChanged();
-                }
-            }
-        };
-
-        mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
-        mViewModel.mTerms.observe(this, termsObserver);
-    }
-
-    private void initRecyclerView() {
-        mTermRecyclerView.setHasFixedSize(true);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        mTermRecyclerView.setLayoutManager(layoutManager);
-
-        DividerItemDecoration divider = new DividerItemDecoration(mTermRecyclerView.getContext(),
-                layoutManager.getOrientation());
-        mTermRecyclerView.addItemDecoration(divider);
     }
 
     @Override
@@ -97,25 +38,7 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_add_sample_data) {
-            addSampleData();
-            return true;
-        } else if (id == R.id.action_delete_all) {
-            deleteAllTerms();
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    private void deleteAllTerms() {
-        mViewModel.deleteAllTerms();
-    }
-
-    private void addSampleData() {
-        mViewModel.addSampleData();
     }
 }
