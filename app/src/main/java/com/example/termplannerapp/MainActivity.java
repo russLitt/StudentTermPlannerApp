57d012872915b0ce50b73b3540839e32b65c830f
Russell Anderson <russellanderson2000@gmail.com>
2020-05-06 19:11:26 -0400

    added a help menu in the main activity to describe how to use app

--- app/src/main/java/com/example/termplannerapp/MainActivity.java
+++ app/src/main/java/com/example/termplannerapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -61,6 +62,28 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_help) {
+            helpMenu();
+            return true;
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void helpMenu() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Learn how to use this app");
+        builder.setMessage("This app is designed to help students track terms and courses " +
+                "as well as assessments and mentors associated with each course." +
+                "\n\nTerms, courses, assessments and mentors can be created in any order, " +
+                "however a term must exist for a course to be added to it and a course " +
+                "must exist for an assessment and or mentors to be added to it." +
+                "\n\nFurthermore, a term cannot be deleted if it has courses assigned to it.");
+        builder.setPositiveButton("Ok", (dialog, id) -> {
+            dialog.dismiss();
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
